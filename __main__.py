a7d27747315c442baf666cbbffc4abe1aa8de301
NanDesuKa-FR <74000964+NanDesuKa-FR@users.noreply.github.com>
2020-11-09 19:09:45 +0100

    add subs only

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+from os import system
+import ctypes
 import io
 from colorama import Fore, Back, Style
 import re
@@ -10,6 +12,7 @@ from sys import platform
 import json
 import sys
 from clint.textui import progress
+import codecs
 
 def Logo():
     if platform == "linux" or platform == "linux2" or platform == "darwin":
@@ -31,7 +34,7 @@ def Logo():
     print(Fore.RED + "                                                                            By NanDesuKa?")
 
 def menu():
-    print("\n\n  Make a choice:\n  ------------------------------------\n  1) Download with link\n  2) exit\n  ------------------------------------")
+    print("\n\n  Make a choice:\n  ------------------------------------\n  1) Download with link\n  2) Download subtitle only\n  3) exit\n  ------------------------------------")
     
 def clean_text(name):
 	return name.replace('|', ' ').replace('>', ' ').replace('<', ' ').replace('"', ' ').replace('?', ' ').replace('?',' ').replace('*', ' ').replace(':', ' ').replace('/', ' ').replace('\\', ' ')
@@ -43,7 +46,7 @@ def checkUrl(url):
     resp = conn.getresponse()
     return resp.status < 400
     
-def Downloader(link):
+def Downloader(link, choix):
     scraper = cloudscraper.create_scraper()
     content = scraper.get(link, headers={'Cache-Control': 'no-cache'}).text
     title = clean_text((content.split('<meta property="og:title"content="')[1]).split('" />')[0])
@@ -52,42 +55,48 @@ def Downloader(link):
             
     JSON = scraper.get("https://www.shadowz.fr/api/player-data/json/" + ID_FOR_JSON, headers={'Cache-Control': 'no-cache'}).text
     data = json.loads(JSON)
-    mp4 = (data["data"]["mp4"]).replace(" ", "%20")
-    hls = (data["data"]["hls"]).replace(" ", "%20")
     
-    print("\n\n  Make a choice:\n  ------------------------------------\n  1) Download MP4\n  2) Download M3U8\n  ------------------------------------")
-    
-    loop = True
-    while loop == True:
-        reponse = input("  Choose 1 or 2: ")
-        if reponse in ['1', '2']:
-            Logo()
-            print()
-            if reponse == "1":
-                r = scraper.get(mp4, headers={'Cache-Control': 'no-cache'}, stream=True)
-                with open(title + '.mp4', 'wb') as f:
-                    total_length = int(r.headers.get('content-length'))
-                    for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
-                        if chunk:
-                            f.write(chunk)
-                            f.flush()
-                loop = False
-            elif reponse == "2":
-                commandvostfr = 'ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "{url_m3u8}" -c copy -bsf:a aac_adtstoasc -y "{outputName}"'.format(outputName=title + ".mp4", url_m3u8=hls)
-                subprocess.call(commandvostfr, shell=True)
-                loop = False
+    if choix.find("1") != -1:
+        mp4 = (data["data"]["mp4"]).replace(" ", "%20")
+        hls = (data["data"]["hls"]).replace(" ", "%20")
+        
+        print("\n\n  Make a choice:\n  ------------------------------------\n  1) Download MP4\n  2) Download M3U8\n  ------------------------------------")
+        
+        loop = True
+        while loop == True:
+            reponse = input("  Choose 1 or 2: ")
+            if reponse in ['1', '2']:
+                Logo()
+                print()
+                if reponse == "1":
+                    r = scraper.get(mp4, headers={'Cache-Control': 'no-cache'}, stream=True)
+                    with open(title + '.mp4', 'wb') as f:
+                        total_length = int(r.headers.get('content-length'))
+                        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+                            if chunk:
+                                f.write(chunk)
+                                f.flush()
+                    loop = False
+                elif reponse == "2":
+                    command = 'ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "{url_m3u8}" -c copy -bsf:a aac_adtstoasc -y "{outputName}"'.format(outputName=title + ".mp4", url_m3u8=hls)
+                    subprocess.call(command, shell=True)
+                    loop = False
 
     if JSON.find('"subtitles":[') != -1:
         subtitle_url = (((((JSON.split('"subtitles":[')[1]).split('],"')[0]).split('"url":"')[1]).split('",')[0]).replace("\\/", "/")).replace(" ", "%20")
-        ass = scraper.get(subtitle_url, headers={'Cache-Control': 'no-cache'}).text
-        txt_to_ass = io.open(title + ".vtt", mode="w", encoding="utf-8")
-        txt_to_ass.write(str(ass))
+        ass = scraper.get(subtitle_url, headers={'Cache-Control': 'no-cache'})
+        ass.encoding = "UTF-8"
+        txt_to_ass = codecs.open(title + ".vtt", "w", "utf-8")
+        txt_to_ass.write(str(ass.text))
         txt_to_ass.close()
+    else:
+        if choix.find("2") != -1:
+            sys.exit("Aucun sous-titre détecter.")
         
     sys.exit("Download complete.")
         
     
-def DownloadByLink():
+def DownloadByLink(choix):
     Logo()
     loop = True
     while loop == True:
@@ -99,23 +108,30 @@ def DownloadByLink():
             
         if link:
             loop = False
-            Downloader(link)
+            Downloader(link, choix)
         else:
             Logo()
-            DownloadByLink()
+            DownloadByLink(choix)
             
 if __name__ == "__main__":
+    if platform == "linux" or platform == "linux2" or platform == "darwin":
+        system("title " + "ShadowZ Downloader v1.1 | By NanDesuka?")
+    elif platform == "win32":
+        ctypes.windll.kernel32.SetConsoleTitleW("ShadowZ Downloader v1.1 | By NanDesuka?")
+        
     Logo()
     menu()
     loop = True
     while loop == True:
-        reponse = input("  Choose 1 or 2: ")
-        if reponse in ['1', '2']:
+        reponse = input("  Choose 1, 2 or 3: ")
+        if reponse in ['1', '2', '3']:
             Logo()
             loop = False
             if reponse == "1":
-                DownloadByLink()
+                DownloadByLink("1")
             elif reponse == "2":
+                DownloadByLink("2")
+            elif reponse == "3":
                 sys.exit("Close by User.")
         else:
             Logo()
